aa3d44fd283b11a2daeda26473f97b4db316c0f5
Brede_e <39888226+bredeespelid@users.noreply.github.com>
2024-07-12 18:11:28 +0200

    Create frontend.js

---
+++ frontend.js
@@ -0,0 +1,15 @@
+{
+  "bindings": [
+    {
+      "name": "myTimer",
+      "type": "timerTrigger",
+      "direction": "in",
+      "schedule": "0 * * * *"
+    }
+  ]
+}
+
+
+{
+  "schedule": "rate(1 hour)"
+}
